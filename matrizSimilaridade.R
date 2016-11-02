02c21f789b8c2dc4ab45475e559cc770ab133eac
sollano <sollanorb@gmail.com>
2016-11-02 16:49:35 -0200

    #6-#12 aceitar multiplas entradas de NI

--- matrizSimilaridade.R
+++ matrizSimilaridade.R
@@ -3,9 +3,13 @@ m.similaridade=function(data, col.especies, col.comparison, rotulo.NI = "NI", in
   # Remover NA
   data = data[!is.na(data[col.especies]),]
   data = data[!is.na(data[col.comparison]),]
-
+  
+  # converter rotulos NI (aplicativo)
+  if(is.null(rotulo.NI)){rotulo.NI <- "NI"}
+  
   # Remover observações cuja espécie é desconhecida
-  semNI = data[data[ ,col.especies] != rotulo.NI,]
+  # modifiquei para aceitar multiplas entradas
+  semNI = data[! data[ ,col.especies] %in% rotulo.NI,]
   
   compair = levels(data[,col.comparison])
   
